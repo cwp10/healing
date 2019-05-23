6068c2365f08f8b1861af45b4a3221953ad8963f
Won Pyoung Choi <cwp10@msn.com>
2019-05-24 01:58:54 +0900

    스크린 터치 이벤트를 게임월드로 전달

---
+++ Assets/Scripts/PlayScene/GameWorld.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class GameWorld : MonoBehaviour
+{
+    private ModelLoadAndSaveUseCase _useCase = null;
+    private UserModel _userModel = null;
+
+    public void OnTap(object sender, object[] args)
+    {
+        _userModel.AddVitality(_userModel.VitalityPerTap);
+        Debug.Log("tap : " + Number.Converter(_userModel.VitalityPerTap) + "   totalVitality : " + Number.Converter(_userModel.TotalVitality));
+    }
+
+    private void Start()
+    {
+        _useCase = new ModelLoadAndSaveUseCase();
+        _userModel = _useCase.GetUser();
+        UserController.Create(_userModel, _useCase);
+    }
+}
---
+++ Assets/Scripts/PlayScene/Views/ScreenView.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class ScreenView : MonoBehaviour
+{
+    [SerializeField] GameEvent tapEvent_ = null;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            tapEvent_.Notify(this, null);
+        }
+    }
+}
